898961ceeea5b218ced7073bc81ece2bf35ea6d3
Adam Oachs <amoachs@hotmail.com>
2023-06-16 23:16:46 -0500

    remove old web app

--- _KnexTelemetryWeb/dist/api.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const router = express.Router();
-router.post('/', function (req, res, next) {
-    res.send(JSON.stringify({ test: "hello" }));
-});
-module.exports = router;
--- _KnexTelemetryWeb/routes/api.ts
+++
@@ -1,8 +0,0 @@
-import express = require('express');
-const router = express.Router();
-
-router.post('/', function(req, res, next) {
-  res.send(JSON.stringify({test: "hello"}));
-});
-
-module.exports = router;
